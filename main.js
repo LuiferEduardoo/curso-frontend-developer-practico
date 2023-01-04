b749a909b83b7c81d2153182cf8da4577bedd31f
Luifer Ortega <luifer01ortegaperez@gmail.com>
2023-01-03 21:40:27 -0500

    Se integran JavaScript y se separa los estilos css del html y se integra el menu del desktpo con el mobile, el carrito de compra y la lista de productos

---
+++ main.js
@@ -0,0 +1,114 @@
+const menuEmail = document.querySelector('.navbar-email').addEventListener('click', toggleDestopMenu);
+const menuHamIcon = document.querySelector('.menu').addEventListener('click', toggleMovileMenu);
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart').addEventListener('click', taggleShoppingCart);
+
+
+const mobileMenu = document.querySelector('.mobile-menu');
+const destopMenu = document.querySelector('.desktop-menu');
+const aside = document.querySelector('.product-detail');
+
+function toggleDestopMenu()
+{
+    const isAsideClosed = aside.classList.contains('inactive');
+    if(!isAsideClosed)
+    {
+        aside.classList.add('inactive');
+    }
+    destopMenu.classList.toggle('inactive');
+}
+function toggleMovileMenu()
+{
+    const isAsideClosed = aside.classList.contains('inactive');
+    if(!isAsideClosed)
+    {
+        aside.classList.add('inactive');
+    }
+    mobileMenu.classList.toggle('inactive');
+}
+function taggleShoppingCart()
+{
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDestopoMenuClosed = destopMenu.classList.contains('inactive');
+    if(!isMobileMenuClosed ||!isDestopoMenuClosed)
+    {
+        mobileMenu.classList.add('inactive');
+        destopMenu.classList.add('inactive');
+    }
+    aside.classList.toggle('inactive');
+
+}
+
+const productList = [];
+
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Laptop',
+    price: 500,
+    image: 'https://images.pexels.com/photos/461064/pexels-photo-461064.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+
+productList.push({
+    name: 'Teclado',
+    price: 20,
+    image: 'https://images.pexels.com/photos/1772123/pexels-photo-1772123.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+
+productList.push({
+    name: 'Monitor',
+    price: 200,
+    image: 'https://images.pexels.com/photos/1029757/pexels-photo-1029757.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+
+productList.push({
+    name: 'Reloj',
+    price: 40,
+    image: 'https://images.pexels.com/photos/1162519/pexels-photo-1162519.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+
+productList.push({
+    name: 'Moto',
+    price: 600,
+    image: 'https://images.pexels.com/photos/5803142/pexels-photo-5803142.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+
+function renderProducts (arr)
+{
+    const cardsContainer = document.querySelector('.cards-container');
+    for (product of arr)
+    {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$ ' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        productInfoDiv.append(productPrice, productName);
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+        productInfoFigure.appendChild(productImgCart);
+        productInfo.append(productInfoDiv,productInfoFigure);
+    
+        productCard.append(productImg, productInfo);
+    
+        cardsContainer.appendChild(productCard);
+    }
+
+}
+renderProducts (productList);
